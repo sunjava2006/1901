623f119370f1b16a6bc5d7ed20ea30efe1b31e68
WangRui <jkwangrui@126.com>
2019-05-15 12:01:25 +0800

    '19-5-15'

--- 1901/java/oop/src/com/thzhima/thread/People.java
+++ 1901/java/oop/src/com/thzhima/thread/People.java
@@ -2,13 +2,35 @@ package com.thzhima.thread;
 
 public class People { 
 	String name;
+	static final String national;
+	
+	final String gender ;
+	
+	static{
+		national = "china";
+	}
+	
+	{
+	  // this.gender = "female";	
+	}
+	
+	public People(String g){
+		this.gender = g;
+	}
+	
 	public void run ()
 	{
 		System.out.println("run....");
 	}
-	public void eat ()
+	public void eat (int a, int b)
 	{
+		int c = a;
 		System.out.println("eat....");
+		{
+			int d = c;
+			System.out.println(a+b+c+d);
+		}
+		//System.out.println(a+b+c+d);
 	}
 	
 }
