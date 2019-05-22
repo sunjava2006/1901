11dc89a27ba245041fe51cecc94b8159500ae36a
wang rui <jkwangrui@126.com>
2019-05-22 09:17:16 +0800

    5-22

--- 1901/java/oop/src/com/thzhima/arrays/Array3.java
+++ 1901/java/oop/src/com/thzhima/arrays/Array3.java
@@ -2,4 +2,12 @@ package com.thzhima.arrays;
 
 public class Array3 {
 
+	public static void main(String[] args) {
+		int[][][] a = new int[][][]{{{2,3}},{{5,6},{6,7,8}}};
+		
+		a[0] = new int[2][2];
+		
+		
+		
+	}
 }
