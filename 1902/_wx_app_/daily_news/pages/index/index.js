5a5423eb76db38cb6b644c24252fed86ac9c1373
WangRui <jkwangrui@126.com>
2019-05-29 17:14:30 +0800

    '5-29'

--- 1902/_wx_app_/daily_news/pages/index/index.js
+++ 1902/_wx_app_/daily_news/pages/index/index.js
@@ -4,12 +4,17 @@ const app = getApp()
 
 Page({
     data: {
-        currId: 0
+        currId: 0,
+        count:3
     },
     selectItem: function(e) {
         var curId = e.target.id;
         
         this.setData({currId: curId});
+    },
+    changeItem:function(e){
+        var id = e.detail.currentItemId;
+        this.setData({currId:id});
     },
     onLoad: function() {
 
