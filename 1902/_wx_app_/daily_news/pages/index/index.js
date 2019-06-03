7f530930c547ad64aef8b5a4387c98c49cca2d83
WangRui <jkwangrui@126.com>
2019-06-03 16:42:44 +0800

    '6-3'

--- 1902/_wx_app_/daily_news/pages/index/index.js
+++ 1902/_wx_app_/daily_news/pages/index/index.js
@@ -1,23 +1,50 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
 
 Page({
     data: {
         currId: 0,
-        count:3
+        count:3,
+        array:null,
+        types: ["top","shehui"]
     },
     selectItem: function(e) {
         var curId = e.target.id;
         
         this.setData({currId: curId});
+        this.list(this.data.types[curId]);
     },
     changeItem:function(e){
         var id = e.detail.currentItemId;
         this.setData({currId:id});
     },
-    onLoad: function() {
+    onShow: function() {
+        console.log("--------onLoad--------------")
+        this.list("top");
+    },
+    list:function(type){
+        var that = this;
+        console.log("----------list -----------");
+        wx.request({
+            url: 'http://v.juhe.cn/toutiao/index',
+            data: {'type':type,
+                'key':'082153265794dc7a4bdb119e272319ee'},
+            header: {'content-type': 'application/json'},
+            method: 'GET',
+            dataType: 'json',
+            responseType: 'text',
+            success: function(res) {
 
+                var list = res.data.result.data;
+                that.setData({"array": list});
+                console.log(list);
+
+            },
+            fail: function(res) {},
+            complete: function(res) {},
+        })
     }
 
-})
+
+});
